0c69bcfa14102ee7f99acfe5b0d5da2c7c272d2b
Ino <masafumi_inoue@dwango.co.jp>
2016-05-25 17:41:33 +0900

    add config switch about TIFF and TGA.and refactoring

--- sources/libcpp83gts_callback_and_action/cb_config_save.cpp
+++ sources/libcpp83gts_callback_and_action/cb_config_save.cpp
@@ -3,39 +3,33 @@
 #include "ptbl_funct.h"
 #include "pri.h"
 #include "gts_master.h"
-#include "gts_gui.h"
 
 void gts_master::cb_config_save( void )
 {
-	char *cp_path;
-
 	/* configファイルへのパス名を得る
 		いままでのloadしたconfigファイルで最新の名前 */
-	cp_path = this->cl_memo_config.cp_memory_path();
-
-	/* cp_pathはNULLを返さない */
-	assert(NULL != cp_path);
+	const std::string filepath( this->cl_memo_config.memory_of_path );
 
 	/* まだconfigファイルをloadしていないとき */
-	if ('\0' == cp_path[0]) {
+	if ( filepath.empty() ) {
 		fl_alert("Not Load Config file yet.");
 		return;
 	}
 
 	/* すでに存在するなら上書き確認のダイオローグを表示 */
-	if (ptbl_dir_or_file_is_exist( cp_path )) {
-		if (0 == fl_ask("Overwrite<%s>?",cp_path)) { return; }
+	if (ptbl_dir_or_file_is_exist( const_cast<char *>(filepath.c_str()) )) {
+		if (0 == fl_ask("Overwrite<%s>?",filepath.c_str())) { return; }
 	}
 	/* 存在しないときは保存確認のダイオローグを表示 */
 	else {
-		if (0 == fl_ask("Save<%s>?",cp_path)) { return; }
+		if (0 == fl_ask("Save<%s>?",filepath.c_str())) { return; }
 	}
 
 	/* config情報を保存する */
-	if (OK != this->cl_memo_config.save( cp_path )) {
+	if (OK != this->cl_memo_config.save( filepath.c_str() )) {
 		pri_funct_err_bttvr(
 	 "Error : this->cl_memo_config.save(%s) returns NG",
-			cp_path );
+			filepath );
 		return;
 	}
 }
