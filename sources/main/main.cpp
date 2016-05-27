656eaf7da14c09b68732d1887b68a761efff5212
Ino <masafumi_inoue@dwango.co.jp>
2016-05-27 11:46:37 +0900

    change version and gitignore

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -128,7 +128,7 @@ namespace {
 }
 
 /* リリース名、版、日付 */
-gts_master cl_gts_master( "gts" ,"2.3.2" ,"2016-5-25" );
+gts_master cl_gts_master( "gts" ,"2.3.3" ,"2016-5-25" );
 
 int main( int argc, char **argv )
 {
