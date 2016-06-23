d6ea905d3f9d6b353ec3ec8106d4e1a6b4d9df7c
Ino <masafumi_inoue@dwango.co.jp>
2016-06-23 11:33:04 +0900

    check date in  main.cpp

--- sources/main/main.cpp
+++ sources/main/main.cpp
@@ -76,7 +76,7 @@ static int argument_analyzer( int argc, char *argv[], char *cp_comm, gts_master 
 # ifdef PACKAGE_NAME
 gts_master cl_gts_master(PACKAGE_NAME, PACKAGE_VERSION, CONFIGURATION_DATE);
 # else
-gts_master cl_gts_master( "gts" ,"2.3.8" ,"2016-6-16" );
+gts_master cl_gts_master( "gts" ,"2.3.8" ,"2016-6-23" );
 # endif
 
 int main( int argc, char **argv )
